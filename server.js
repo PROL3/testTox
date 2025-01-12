88a92e02e0c0db2c1d7a4c29d87a48e2e397b31a
liav <161258307+PROL3@users.noreply.github.com>
2025-01-12 23:46:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch');
-
+import fetch from 'node-fetch';
 const app = express();
 const PORT = 3000;
 
